b9aa3b69f0b2df0b66ea50e8fb0aa530ba3bc7d5
kalairanivignesh <kalairani.vignesh@gmail.com>
2022-04-03 17:46:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as pt
 import streamlit as st
+import plotly.graph_objects as go
 st.set_page_config(page_title="Hackaiz-2022",page_icon="UA", layout="wide", initial_sidebar_state="expanded",
      menu_items={
          'About': "The app is created by [Kalairani](https://kalairani.github.io/) and [Murugesh](https://murugeshmanthiramoorthi.github.io/). This is a project created as a part of HackaViz2022 organised by Toulouse Data Viz."
@@ -11,7 +12,7 @@ df=pd.read_csv("actions_culturelles_soutenues.csv")
 #Filter
 with st.sidebar:
     selected_ouevre=st.radio("Chosir l'oeuvre ici: ", list(df["oeuvre"].unique()))
-    st.snow()
+
 
 
 ## extract only film
@@ -83,6 +84,26 @@ else:
     new_col2.text("")
     new_col2.markdown("""Le montant moyen a <span style="color: #ff0000">diminué</span> pour l'oeuvre <span style="color: #ff0000">"""+ selected_ouevre + """</span> après la Covid-19""", unsafe_allow_html=True)
 
+df_matrix=pd.crosstab(df_film['aide'], df_film['annee'],values=df_film['montant'],aggfunc=sum).fillna(0)
+label=['Création','Diffusion du spectacle vivant','Formation des artistes','Éducation artistique et culturelle',
+       '2016','2017','2018','2019','2020']
+source=[0,0,0,0,0,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3]
+target=[4,5,6,7,8,4,5,6,7,8,4,5,6,7,8,4,5,6,7,8]
+value=[17660911,19647095,21783893,22235770,21509008,653697,824289,1369844,1473316,1201285,362670,407000,380378,417474
+      ,348000,3273,37534,55693,13308,0]
+
+source = source
+target = target
+value = value
+link = dict(source = source, target = target, value = value,color="#fcba03")
+node=dict(label=label , pad=35, thickness=10, color=["#fc4e03","#fc6f03","#fc8c03","#fcc203","#fcf403","#dffc03","#cefc03","#b1fc03","#90fc03"])
+data = go.Sankey(link = link, node=node)
+
+fig_sankey = go.Figure(data)
+
+st.subheader("Contribution pour les aides")
+st.plotly_chart(fig_sankey, use_container_width=True)
+
 
 col1, col2 = st.columns ([4,4])
 ## bar chart
@@ -100,13 +121,15 @@ col1.subheader("Le montant distribué par collecteur")
 col1.plotly_chart(fig, use_container_width=True)
 
 
-fig_scatter = pt.scatter(df_film, x="annee", y="montant", color="aide",
-                labels={'aide':'Aide', 'annee':'Annee', 'montant': 'Montant', 'collecteur':'Collecteur'},
-                color_discrete_sequence=["#FFC300", "#BF7A31","#FF5733","#C70039"],
-                 size='montant', hover_data=['collecteur'], size_max=20)
 
-fig_scatter.update_layout(paper_bgcolor='rgba(0,0,0,0)',
-    plot_bgcolor='rgba(0,0,0,0)'
+
+fig_2=pt.scatter(df_film, x="montant", y="aide", animation_frame="annee",color="collecteur",size="montant"
+                 ,labels={'collecteur':'Collecteur', 'annee':'Annee', 'montant': 'Montant','aide':'Aide'})
+
+fig_2.update_layout(paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',transition = {'duration': 2000}
 )
+
 col2.subheader("Le montant distribué par aide")
-col2.plotly_chart(fig_scatter, use_container_width=True)
+col2.plotly_chart(fig_2, use_container_width=True)
+

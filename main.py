530898ec4b57d2127677b954555e3c27f6acf7ec
kalairanivignesh <kalairani.vignesh@gmail.com>
2022-04-03 18:56:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as pt
 import streamlit as st
+import numpy as np
 import plotly.graph_objects as go
 st.set_page_config(page_title="Hackaiz-2022",page_icon="UA", layout="wide", initial_sidebar_state="expanded",
      menu_items={
@@ -16,8 +17,8 @@ with st.sidebar:
 
 
 ## extract only film
-df_film=df[df['oeuvre']==selected_ouevre]
-bar_chart=df_film.groupby(by=["collecteur","annee"]).sum().sort_values(by="montant", ascending=False).reset_index()
+df_filter=df[df['oeuvre']==selected_ouevre]
+bar_chart=df_filter.groupby(by=["collecteur","annee"]).sum().sort_values(by="montant", ascending=False).reset_index()
 bar_chart["annee"]=bar_chart["annee"].round(0).astype('object')
 
 
@@ -42,18 +43,18 @@ st.write(
     """,
     unsafe_allow_html=True,
 )
-m1.metric(label ='💸 Montant total',value = str(round(df_film["montant"].sum()/1000000,2)) + ' M €',
-          delta=str(round(((1 - (542000000 - df_film["montant"].sum()) / 542000000)) * 100, 2)) + ' % de entier',
+m1.metric(label ='💸 Montant total',value = str(round(df_filter["montant"].sum()/1000000,2)) + ' M €',
+          delta=str(round(((1 - (542000000 - df_filter["montant"].sum()) / 542000000)) * 100, 2)) + ' % de entier',
           delta_color='normal')
-m2.metric(label ='❤️ Nombre total des beneficiaires',value = str(df_film["beneficiaire"].nunique()),
-          delta=str(round(((1 - (22000 - df_film["beneficiaire"].nunique()) / 22000)) * 100, 2)) + ' % de entier',
+m2.metric(label ='❤️ Nombre total des beneficiaires',value = str(df_filter["beneficiaire"].nunique()),
+          delta=str(round(((1 - (22000 - df_filter["beneficiaire"].nunique()) / 22000)) * 100, 2)) + ' % de entier',
           delta_color='normal')
-m3.metric(label ='🧮 Nombre total des projets',value = str(df_film.shape[0]),
-          delta=str(round(((1 - (53003- df_film.shape[0])/53003))*100, 2)) + ' % de entier',
+m3.metric(label ='🧮 Nombre total des projets',value = str(df_filter.shape[0]),
+          delta=str(round(((1 - (53003- df_filter.shape[0])/53003))*100, 2)) + ' % de entier',
               delta_color='normal')
 
-a_covid = df_film[df_film["annee"]==2020]["montant"].mean()
-b_covid = df_film[df_film["annee"]<2020]["montant"].mean()
+a_covid = df_filter[df_filter["annee"]==2020]["montant"].mean()
+b_covid = df_filter[df_filter["annee"]<2020]["montant"].mean()
 
 if a_covid > b_covid:
     new_col1, new_col2 = st.columns([1,1])
@@ -84,17 +85,20 @@ else:
     new_col2.text("")
     new_col2.markdown("""Le montant moyen a <span style="color: #ff0000">diminué</span> pour l'oeuvre <span style="color: #ff0000">"""+ selected_ouevre + """</span> après la Covid-19""", unsafe_allow_html=True)
 
-df_matrix=pd.crosstab(df_film['aide'], df_film['annee'],values=df_film['montant'],aggfunc=sum).fillna(0)
-label=['Création','Diffusion du spectacle vivant','Formation des artistes','Éducation artistique et culturelle',
-       '2016','2017','2018','2019','2020']
-source=[0,0,0,0,0,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3]
-target=[4,5,6,7,8,4,5,6,7,8,4,5,6,7,8,4,5,6,7,8]
-value=[17660911,19647095,21783893,22235770,21509008,653697,824289,1369844,1473316,1201285,362670,407000,380378,417474
-      ,348000,3273,37534,55693,13308,0]
+df_matrix=pd.crosstab(df_filter['aide'], df_filter['annee'],values=df_filter['montant'],aggfunc=sum).fillna(0)
+label=list(df_matrix.index) + list(df_matrix.columns)
+source=[]
+for i in range(len(df_matrix.index)):
+    source.extend([i] * len(df_matrix.columns))
 
-source = source
-target = target
-value = value
+
+
+target = list(np.arange(label.index(list(df_matrix.columns)[0]),label.index(list(df_matrix.columns)[-1])+1))*len(df_matrix.columns)
+
+value=[]
+for i in df_matrix.iterrows():
+    value.extend(list(i[1]))
+
 link = dict(source = source, target = target, value = value,color="#fcba03")
 node=dict(label=label , pad=35, thickness=10, color=["#fc4e03","#fc6f03","#fc8c03","#fcc203","#fcf403","#dffc03","#cefc03","#b1fc03","#90fc03"])
 data = go.Sankey(link = link, node=node)
@@ -123,7 +127,7 @@ col1.plotly_chart(fig, use_container_width=True)
 
 
 
-fig_2=pt.scatter(df_film, x="montant", y="aide", animation_frame="annee",color="collecteur",size="montant"
+fig_2=pt.scatter(df_filter, x="montant", y="aide", animation_frame="annee",color="collecteur",size="montant"
                  ,labels={'collecteur':'Collecteur', 'annee':'Annee', 'montant': 'Montant','aide':'Aide'})
 
 fig_2.update_layout(paper_bgcolor='rgba(0,0,0,0)',
